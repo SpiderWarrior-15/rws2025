5ea6b417120dd8150faedb1e192fd6f8afe5afbe
SpiderWarrior-15 <spiderwarrior15@gmail.com>
2025-08-02 16:55:26 +0530

    Updated Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
-import Login from './pages/Login';
 import {
   Eye,
   EyeOff,
